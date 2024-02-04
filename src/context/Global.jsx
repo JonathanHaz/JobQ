e7dd3bbddbce6227d7245c4db7768f37722d843c
sunnyAonep <arbiv.sunny@gmail.com>
2024-02-04 11:05:41 +0200

    Global Changes

--- src/context/Global.jsx
+++ src/context/Global.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
+import { auth, db } from '../config/firebase';
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
+export const userContext = createContext(); 
 
 export default function Global() {
+    const [user , setUser] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
   return (
     <div>
       
