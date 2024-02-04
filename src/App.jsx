6f2374a7e7a5c27a878c11c5d33a3ed78d6e9f15
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-02-04 12:35:21 +0200

    Aded Auth and JobFinder Links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navigation/Navbar";
+import Auth from './pages/Auth/Auth'
+import JobFinder from "./pages/JobFinder/JobFinder";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
@@ -10,6 +12,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/search" element={<JobFinder />} />
         </Routes>
       </BrowserRouter>
     </>
