6e32dec6d80c06d12f64001085dd46532d83d382
sunnyAonep <arbiv.sunny@gmail.com>
2024-02-04 14:27:52 +0200

    gg

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,9 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa'; // Import the menu icon
+import { FaBars } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -14,40 +11,9 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
-        <li>
-          <Link to="/auth">Auth</Link>
-        </li>
-        <li>
-          <Link to="/search">Search</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/hr">HR</Link>
-        </li>
-        <li>
-          <Link to="/all-profiles">All Profiles</Link>
-        </li>
-      </ul>
-    </nav>
-
     <div>
-      {/* Sidebar toggle icon */}
       <FaBars className="menu-icon" onClick={toggleSidebar} />
 
-      {/* Sidebar */}
       <nav className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
         <ul className="side-list">
           <li><Link to="/">Home</Link></li>
@@ -56,7 +22,6 @@ const Navbar = () => {
         </ul>
       </nav>
 
-      {/* Main Navbar */}
       <nav className="main-navbar">
         <ul className="nav-list">
           <li><Link to="/favorites">Favorites</Link></li>
