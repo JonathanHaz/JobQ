f81daf562920cc483a4219282a35e772181c1bc2
sunnyAonep <arbiv.sunny@gmail.com>
2024-02-04 13:49:25 +0200

    fix the auth

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { userContext } from '../../context/Global'
-
-export default function SignUp() {
+import { Link } from 'react-router-dom';
+import "./Auth.css"
+export default function SignUp(props) {
     const { handleEmailChange, handlePasswordChange, handleSignUp , handleUserStatusChange} = useContext(userContext);
     const [passwordOne, setPasswordOne] = useState('');
     const [passwordTwo, setPasswordTwo] = useState('');
@@ -25,15 +26,22 @@ export default function SignUp() {
     };
   
   return (
+    <div className='signForm'>
     <form onSubmit={PasswordCheck}>
         <input type="text" placeholder='Email' name='Email'  onChange={handleEmailChange}/>
         <input type="password" name="password" onInput={handlePasswordTwoCheck} onChange={handlePasswordChange} />
         <input type="password" name="password1" onInput={handlePasswordCheck} />
-        <input type="radio" id="jobSeeker" name="userType" value="jobSeeker" onChange={handleUserStatusChange}/>
-         <label htmlFor="jobSeeker">job seeker</label>
-         <input type="radio" id="HR" name="userType" value="HR" onChange={handleUserStatusChange}/>
-        <label htmlFor="HR">HR</label>
+        <div class="radio-container">
+    <input type="radio" id="jobSeeker" name="userType" value="jobSeeker" onChange={handleUserStatusChange} />
+    <label class="radio-label" htmlFor="jobSeeker"><div class="radio-btn"></div> Job Seeker</label>
+    </div>
+    <div class="radio-container">
+    <input type="radio" id="HR" name="userType" value="HR" onChange={handleUserStatusChange} />
+    <label class="radio-label" htmlFor="HR"><div class="radio-btn"></div> HR</label>
+    </div>
         <button>Sign-Up</button>
     </form>
+    <p>Already have an account? <Link to="/auth" onClick={props.toggleForm}>Login</Link></p>
+    </div>
   )
 }
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,14 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Login from '../../components/Auth/Login'
 import SignUp from '../../components/Auth/SignUp'
 import { userContext } from '../../context/Global'
 
 export default function Auth() {
   const {handleSignOut} = useContext(userContext)
+  const [signButton, setSignButton] = useState(false);
+  const toggleForm = ()=>{
+    setSignButton(!signButton)
+  }
   return (
     <div>
-      <Login/>
-      <SignUp/>
+      {signButton? 
+      <Login toggleForm={toggleForm}/>
+      :
+      <SignUp toggleForm={toggleForm}/>
+      }
+      
+      
       <div>
         <button onClick={handleSignOut} className='Sign-button'>SignOut</button>
      </div>
