1923eafccc6644ba0f52421f784dcb8bc2169bfe
urielbang <uri32655@gmail.com>
2024-02-04 14:09:09 +0200

    uriel5

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/favorites">Favorites</Link></li>
-        <li><Link to="/auth">Auth</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/hr">HR</Link></li>
-        <li><Link to="/all-profiles">All Profiles</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+        <li>
+          <Link to="/auth">Auth</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/hr">HR</Link>
+        </li>
+        <li>
+          <Link to="/all-profiles">All Profiles</Link>
+        </li>
       </ul>
     </nav>
   );
