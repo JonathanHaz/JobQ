654f3721012ca6bd8e6fac02d6809dbfd3c21976
urielbang <uri32655@gmail.com>
2024-02-04 11:41:22 +0200

    uriel3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Home from "./pages/Home/Home";
+
 function App() {
   return (
     <>
-    <BrowserRouter>
-   <Navbar/>
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-   </Routes>
-   </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
