d802261702bb94cbefd927923558c91d6e548da3
sunnyAonep <arbiv.sunny@gmail.com>
2024-02-04 18:52:37 +0200

    upload

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import UploadFiles from '../../components/Profile/UploadFiles'
+import UploadImg from '../../components/Profile/UploadImg'
 
 export default function Profile() {
   return (
     <div>
      <UploadFiles/>
+     <br />
+     <UploadImg/>
     </div>
   )
 }
