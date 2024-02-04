a26c6389611a7e5a634db3b68974658cb802de07
urielbang <uri32655@gmail.com>
2024-02-04 17:27:56 +0200

    uriel9

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,62 +1,66 @@
-<<<<<<< HEAD
-import React from "react";
+import React, { useContext, useState } from "react";
+
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import "./Navbar.css";
-
-
-import React, { useState } from 'react';
-=======
-import React, { useContext, useState } from 'react';
->>>>>>> 8de0d22f39c0c37e34087204bbfab055b8741437
-import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
-import './Navbar.css';
-import { userContext } from '../../context/Global'
-
+import { userContext } from "../../context/Global";
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  
-  const { handleSignOut,user } = useContext(userContext)
+
+  const { handleSignOut, user } = useContext(userContext);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
   return (
-
-
-
     <div>
       <FaBars className="menu-icon" onClick={toggleSidebar} />
 
-      <nav className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+      <nav className={`sidebar ${sidebarOpen ? "open" : ""}`}>
         <ul className="side-list">
-          {user?
-          <>      
-          <li><Link to="/favorites">Favorites</Link></li>
-          <li><Link to="/search">Search</Link></li>
-          <li><Link to="/hr">HR</Link></li>
-          <li><Link to="/all-profiles">All Profiles</Link></li> 
-          </>
-          :
-          <>
-          <li><Link to="/">Auth</Link></li>
-          <li><Link to="/home">Home</Link></li>
-          </>
-          }
+          {user ? (
+            <>
+              <li>
+                <Link to="/favorites">Favorites</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/hr">HR</Link>
+              </li>
+              <li>
+                <Link to="/all-profiles">All Profiles</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/">Auth</Link>
+              </li>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
 
       {/* Main Navbar */}
       <nav className="main-navbar">
         <ul className="nav-list">
-          {user?<button onClick={handleSignOut} className='Sign-button'>Log Out</button> : <Link to='/auth  '>Sign In</Link>}
-        
+          {user ? (
+            <button onClick={handleSignOut} className="Sign-button">
+              Log Out
+            </button>
+          ) : (
+            <Link to="/auth  ">Sign In</Link>
+          )}
         </ul>
       </nav>
     </div>
-
   );
 };
 
