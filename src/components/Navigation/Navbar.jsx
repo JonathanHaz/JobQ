4e9ec9c98c4fc97f660f80ed6a9ec028aee206b6
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-02-04 11:23:03 +0200

    NavBar

---
+++ src/components/Navigation/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Create a CSS file for styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/favorites">Favorites</Link></li>
+        <li><Link to="/auth">Auth</Link></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/hr">HR</Link></li>
+        <li><Link to="/all-profiles">All Profiles</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+       <div className="hero-content">
+    <h1>Empower Your Career Quest with JobQuest</h1>
+    <p>Welcome to JobQuest, your premier destination for navigating the job market with ease and precision...</p>
+    <button>Start Your Quest</button>
+  </div>
+  <div className="important">
+    <h2>What makes us different</h2>
+    <p></p>
+    <p></p>
+    <p></p>
+    <div>
+    <p>Build your career</p>
+    <p></p>
+    <p></p>
+    </div>
+  </div>
+  <footer className="footer">
+    <div>Quick Links: Home | About Us | Search Jobs | Contact Us | FAQs</div>
+    <div>Follow Us on Social Media: [LinkedIn, Twitter, Facebook, Instagram]</div>
+    <div>Contact Information: Get in Touch with JobQuest - info@jobquest.com</div>
+    <div>© {new Date().getFullYear()} JobQuest. All rights reserved.</div>
+  </footer>
+    </div>
+  )
+}
