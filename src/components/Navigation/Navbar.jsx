1fa8c13e18789288f572e42deaaf22aa32a6a7f3
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-02-04 14:17:46 +0200

    Navbar Update

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState,useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa'; // Import the menu icon
 import './Navbar.css';
+import { userContext } from '../../context/Global'
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+  const { handleSignOut,user } = useContext(userContext)
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -21,17 +24,19 @@ const Navbar = () => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/favorites">Favorites</Link></li>
+          <li><Link to="/auth">Auth</Link></li>
+          <li><Link to="/search">Search</Link></li>
+          <li><Link to="/hr">HR</Link></li>
+          <li><Link to="/all-profiles">All Profiles</Link></li>
         </ul>
       </nav>
 
       {/* Main Navbar */}
       <nav className="main-navbar">
         <ul className="nav-list">
-          <li><Link to="/favorites">Favorites</Link></li>
-          <li><Link to="/auth">Auth</Link></li>
-          <li><Link to="/search">Search</Link></li>
-          <li><Link to="/hr">HR</Link></li>
-          <li><Link to="/all-profiles">All Profiles</Link></li>
+          {user?<button onClick={handleSignOut} className='Sign-button'>Log Out</button> : <Link to='/auth  '>Sign In</Link>}
+        
         </ul>
       </nav>
     </div>
