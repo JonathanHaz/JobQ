41794f755b67c724fd2544b25839ba6b37f1793b
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-02-05 00:35:03 +0200

    Styled Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext,useEffect,useState } from 'react';
+import { FaGithub, FaLinkedin, FaFacebook,FaRobot } from 'react-icons/fa';
 import { userContext } from '../../context/Global'
 import './Home.css'; // Create a CSS file for styling
 import ai from '../../assets/Images/Ai.jpg'
@@ -10,17 +11,23 @@ import ChatBot from 'react-simple-chatbot';
 
 export default function Home() {
   const { user } = useContext(userContext)
+  const [chatbotOpen, setChatbotOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setChatbotOpen(!chatbotOpen);
+  };
+  
   return (
     <div className="home-container">
       <div className="hero-content">
         <h1>Empower Your Career Quest with JobQuest</h1>
         <p>Welcome to JobQuest, your premier destination for navigating the job market with ease and precision...</p>
-        <button>
+        <button className='startedBTN'>
           {user ?(<Link to='/search'>Get Started</Link>):(<Link to='/'>Get Started</Link>) }
           </button>
       </div>
       <div className="different">
-        <h2>What makes us different</h2>
+        <h2>What makes us different?</h2>
         <div className="different-features">
           <div className="feature">
             <img src={ai} alt="Feature 1" />
@@ -38,12 +45,20 @@ export default function Home() {
       </div>
       <div className='contact-us'>
         <h2>Contact Us</h2>
-        <a href="">Github</a>
-        <a href="">LinkedIn</a>
-        <a href="">Facebook</a>
-      </div>
-      <ChatBot
-
+        <div className='contact-links'>
+          <a href="">
+            <FaGithub />
+          </a>
+          <a href="">
+            <FaLinkedin />
+          </a>
+          <a href="">
+            <FaFacebook />
+          </a>
+        </div>
+      {chatbotOpen && (
+  <ChatBot className="Bot"
+  
   steps={[
     {
       id: "1",
@@ -94,7 +109,10 @@ export default function Home() {
       end: true,
     },
   ]}
-/>
+  />
+)}
+      <button className='botBTN' onClick={toggleChatbot}><FaRobot /></button>
+      </div>
       <footer className="footer">
         <div>© {new Date().getFullYear()} JobQuest. All rights reserved.</div>
       </footer>
