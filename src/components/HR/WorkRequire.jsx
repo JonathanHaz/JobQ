ca270a078da312699bbf5ea815beffb833d5c7f2
sunnyAonep <arbiv.sunny@gmail.com>
2024-02-05 03:00:52 +0200

    hr edit

--- src/components/HR/WorkRequire.jsx
+++ src/components/HR/WorkRequire.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 export default function WorkRequire(props) {
   return (
     <div>
-      <input className="form-group" id="requires" placeholder="requires" name={`requires${props.currentWork}`} onChange={props.handleChange} />
+        <Box
+              sx={{
+                '& .MuiTextField-root': { m: 1, maxWidth: '100%' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+        <TextField
+          id="outlined-multiline-static requires"
+          name={`requires${props.currentWork}`}
+          label="requires"
+          multiline
+          onChange={props.handleChange}/>   
+        </Box>
     </div>
   );
 }
